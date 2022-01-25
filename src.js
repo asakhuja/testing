983ffe0a8ec243ba90aec7059a9d4910e84e01fe
AppSecure <appsecure@AppSecures-MacBook-Air.local>
2022-01-25 23:06:27 +0530

    2commit

--- src.js
+++ src.js
@@ -4,3 +4,6 @@ var testpush = "AKIAWDPZFS7T5QEUSWMT"
 var t = "p8e-amamamamam2929292929sjsjsjsjsj55"
 
 var a="adobelogin adobe.io id secret"
+
+var b = "ababababab1010101010a0a0a0a0a012"
+var c = "p8e-amamamamam2929292929sjsjsjsjsj55"
